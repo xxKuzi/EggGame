1e555e2b45905896bb166d9f16b8bfe39d44d561
xxKuzi <jakub.kuzel7@gmail.com>
2023-04-20 14:59:53 +0200

    only update

---
+++ Assets/Scripts/AdButtonScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdButtonScript : MonoBehaviour
+{
+    
+    
+
+    private void Awake()
+    {
+        
+        gameObject.GetComponent<Button>().onClick.AddListener(RewardedButtonClick);
+        
+    }
+
+    void RewardedButtonClick()
+    {
+        GoogleAdMobController.Instance.ShowRewardedAd();
+    }
+    
+
+    
+}

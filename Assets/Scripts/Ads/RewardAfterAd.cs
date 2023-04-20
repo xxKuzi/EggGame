4f9bd1c30cb3a92a2ce6656f53d4c66cdb773b96
xxKuzi <jakub.kuzel7@gmail.com>
2023-04-20 14:42:15 +0200

    1

--- Assets/Scripts/Ads/RewardAfterAd.cs
+++ Assets/Scripts/Ads/RewardAfterAd.cs
@@ -33,8 +33,6 @@ public class RewardAfterAd : MonoBehaviour
         }
         else
         {
-            Debug.Log("NOT GAMEMENu");
-            GoogleAdMobController.Instance.DestroyBannerAd();
             GoogleAdMobController.Instance.HideBannerAd();
         }
 
